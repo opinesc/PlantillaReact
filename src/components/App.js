75155880efab1d5e15bf046ec43f85b4d4ec9bc3
opinesc <omarpinera@gmail.com>
2019-01-02 19:46:08 +0100

    Add files via upload
    Plantilla React webpack y babel

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                <i className="fas fa-angle-down"></i>    
+                <h1>Opinesc App!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;

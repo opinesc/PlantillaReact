e0740973b93c29484ea4cf6b063b59a322425a45
opinesc <omarpinera@gmail.com>
2019-01-02 21:50:40 +0100

    Update webpack.config.js
    Añadimos que se cree un archivo css (ExtractTextPlugin)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -15,12 +16,19 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
-            }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                })
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template:'./src/index.html'
-        })
+        }),
+        new ExtractTextPlugin("styles.css")
     ]
 }
